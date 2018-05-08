af6fca1614e56f8a94b29e264003b5762a004cda
andresasdrubal <andresyou26@hotmail.com>
2018-05-07 19:15:54 -0500

    Add files via upload

---
+++ peaje.ino.ino
@@ -0,0 +1,56 @@
+#include <Servo.h>
+ #define Pecho 11
+#define Ptrig 12
+long duracion, distancia; 
+int ledrojo= 6;
+int ledverde= 7;
+
+// Declaramos la variable para controlar el servo
+Servo servoMotor;
+ 
+void setup() {
+  // Iniciamos el monitor serie para mostrar el resultado
+  Serial.begin(9600);
+pinMode(ledrojo, OUTPUT);
+pinMode(ledverde, OUTPUT);
+ 
+  // Iniciamos el servo para que empiece a trabajar con el pin 9
+  servoMotor.attach(9);
+  servoMotor.write(0);
+
+  //ultrasonico
+    pinMode (Pecho, INPUT);
+   pinMode (Ptrig, OUTPUT);
+}
+
+void loop() {
+   digitalWrite(Ptrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Ptrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Ptrig, LOW);
+
+   duracion = pulseIn(Pecho, HIGH);
+  distancia = (duracion/2)/29;
+   Serial.print(distancia);
+  Serial.println("cm");
+  if ((distancia >5 ) or (distancia<0))
+  {
+     servoMotor.write(0);
+digitalWrite(ledrojo, HIGH);
+digitalWrite(ledverde, LOW);
+    // Esperamos 1 segundo
+     delay(1000);
+  
+  }
+  else
+  {
+    // Desplazamos a la posición 180º
+     servoMotor.write(90);
+digitalWrite(ledverde, HIGH);
+digitalWrite(ledrojo, LOW);
+    // Esperamos 1 segundo
+    delay(3000);
+  }
+
+}
---
+++ sensor_temperatura.ino
@@ -0,0 +1,41 @@
+
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(7, 8, 9, 10, 11 , 12);
+
+const int sensorPin= A5;
+
+void setup() {
+
+Serial.begin(9600);
+
+lcd.begin(16,2);
+
+lcd.setCursor(0,0);
+
+lcd.print("TEMPERATURA");
+
+}
+
+void loop() {
+
+int value = analogRead(sensorPin);
+
+float millivolts = (value / 1023.0) * 300;
+
+float celsius = millivolts / 10;
+
+lcd.setCursor(0,1);
+
+Serial.print(celsius);
+
+Serial.println(" C");
+
+lcd.print(celsius);
+
+lcd.print(" C");
+
+delay(300);
+
+}
+
+
